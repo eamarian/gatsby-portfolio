26924030e1a24403646df0c8afd8878c04ef30a0
eamarian <eamarian@gmail.com>
2023-04-09 15:11:18 -0400

    More scroll reveal

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import sr, { srConfig } from "../../utils/sr";
+import { usePrefersReducedMotion } from "../../hooks";
 
 export default ((): React.ReactElement => {
+  const revealSection: React.RefObject<HTMLElement> = useRef<HTMLElement>(null);
+  const prefersReducedMotion: boolean = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (!prefersReducedMotion && sr && revealSection.current) {
+      sr.reveal(revealSection.current, srConfig());
+    }
+  }, []);
   return (
-    <section id="contact">
+    <section id="contact" ref={revealSection}>
       <h1>Contact Me</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque odio eum
